caa5c2da5495da461666c7fb8613e3e9972dfa0f
Jebershon <jebershon100@gmail.com>
2024-01-02 10:44:36 +0530

    0.0.0.2.3

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Signin from './Components/Signin.jsx';
 import Signup from './Components/Signup.jsx';
 function App() {
   return (
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL="/"}>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/Glocery/*" element={<Glocery/>} />

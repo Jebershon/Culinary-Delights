30384380f1f92ce063c878f96a4b6f2febf9f0de
Jebershon <jebershon100@gmail.com>
2024-01-02 10:47:23 +0530

    0.0.0.2.4

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 import Aboutus from './Components/Aboutus';
 import Cart from './Components/Cart-Details.jsx';
 import Glocery from './Components/Glocery.jsx';
-import Home from './Components/Home';
+import Home from './Components/Home.jsx';
 import MyProfile from './Components/MyProfile.jsx';
-import NutritionCalculator from './Components/NutritionCalculator';
-import Recipes from './Components/Recipes';
+import NutritionCalculator from './Components/NutritionCalculator.jsx';
+import Recipes from './Components/Recipes.jsx';
 import Signin from './Components/Signin.jsx';
 import Signup from './Components/Signup.jsx';
 function App() {

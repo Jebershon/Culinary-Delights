9c9202bcf92fc315943e4b888a02dc6574dba2a5
Jebershon <jebershon100@gmail.com>
2024-01-29 13:49:38 +0530

    0.0.2.3

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -31,7 +31,7 @@ function Home() {
 <center>
         <Row className='mt-4 p-2'>
           <Col lg={3}>
-          <img src='https://preview.colorlib.com/theme/cuisines/images/img_4.jpg' width="230px" height="200px" alt=""></img>
+          <img src='https://preview.colorlib.com/theme/cuisines/images/img_4.jpg' width="230px" height="200px" alt="image of food"></img>
           </Col>
 
           <Col lg={3} data-aos="fade-up" data-aos-duration="2000" className='mt-3 mb-3'>
@@ -43,7 +43,7 @@ function Home() {
           </Col>
 
           <Col lg={3}>
-          <img src='https://preview.colorlib.com/theme/cuisines/images/img_1.jpg' width="230px" height="200px" alt=""></img>
+          <img src='https://preview.colorlib.com/theme/cuisines/images/img_1.jpg' width="230px" height="200px" alt="image of food"></img>
           </Col>
 
           <Col lg={3} data-aos="fade-up" data-aos-duration="2000" className='mt-3 mb-3'>
@@ -67,7 +67,7 @@ function Home() {
           </Col>
 
           <Col lg={3}>
-          <img src='https://preview.colorlib.com/theme/cuisines/images/img_2.jpg' width="230px" height="200px" alt=""></img>
+          <img src='https://preview.colorlib.com/theme/cuisines/images/img_2.jpg' width="230px" height="200px" alt="image of food"></img>
           </Col>
 
           <Col lg={3} data-aos="fade-up" data-aos-duration="2000" className='mt-3 mb-3'>
@@ -79,7 +79,7 @@ function Home() {
           </Col>
 
           <Col lg={3}>
-          <img src="https://preview.colorlib.com/theme/cuisines/images/img_3.jpg" width="230px" height="200px" alt="" />
+          <img src="https://preview.colorlib.com/theme/cuisines/images/img_3.jpg" width="230px" height="200px" alt="image of food" />
           </Col>
         </Row>
 </center>
@@ -138,13 +138,13 @@ function Home() {
         <Link  class="primary-btn" to='./Recipes'>view full menu</Link>
         </Col>
         <Col lg={6}  data-aos="fade-up">
-        <img className="img-fluid mt-2 mb-2" src="https://preview.colorlib.com/theme/marco/img/about-img.jpg" alt="image"/>
+        <img className="img-fluid mt-2 mb-2" src="https://preview.colorlib.com/theme/marco/img/about-img.jpg" alt="image of food"/>
         </Col>
         </Row>
 
         <Row className='mt-3 mb-3 p-1' style={{backgroundColor:"black",borderRadius:'20px'}}>
         <Col lg={6}   data-aos="fade-up">
-        <img className="img-fluid mt-2 mb-2" src="https://img.freepik.com/free-photo/healthy-vegetables-wooden-table_1150-38014.jpg" alt="image"/>
+        <img className="img-fluid mt-2 mb-2" src="https://img.freepik.com/free-photo/healthy-vegetables-wooden-table_1150-38014.jpg" alt="image of food"/>
         </Col>
         <Col lg={6} className='home-left p-5'   data-aos="fade-up" data-aos-duration="2000">
         <h1 className='h1-tag'>About Our Story</h1>
